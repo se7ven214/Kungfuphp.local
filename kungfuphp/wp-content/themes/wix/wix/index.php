2de3c325928a312424b762aabdd066ba9c8d3ef5
se7ven214 <kt-duynt@KT-DUYNTs-MacBook-Pro.local>
2015-07-28 18:07:04 +0700

    home
    home page

--- kungfuphp/wp-content/themes/wix/wix/index.php
+++ kungfuphp/wp-content/themes/wix/wix/index.php
@@ -3,23 +3,32 @@
  * The main template file.
  */	
 get_header();
+query_posts( 'posts_per_page=6' );
+$i=1;
 ?>
 <!-- Details Start  -->
 <section class="detail-section">
 	<div class="container">
     	<div class="row details-mian">
-        	<?php
-			get_sidebar();
-			?>
-            <article class="col-md-8 no-padding-left">
+            
+            <article class="col-md-8 no-padding-left" >
+                <h1>
+                    Bài viết mới nhất
+                    <span class="arrow"></span>
+                </h1>
             	<div class="detail-inner masonry-container">
-                <?php  if ( have_posts() ) :  while ( have_posts() ) : the_post(); ?>              
-                    <div class="col-md-6 box">
+                <?php  if ( have_posts() ) :  while ( have_posts() ) : the_post(); ?>  
+                        <?php 
+                            $style = ($i==5 || $i==6) ? "":"border-bottom: 1px solid #e6e6e6;";
+                            $i++;
+                        ?>            
+                    <div class="col-md-4 box" style="<?php echo $style; ?>height:150px;">
                     	<div class="article">
                         	<?php $wix_feat_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID)); ?>
-                        	<div class="post-box-img">
+                        	<div class="post-box-img" style="width:100px;float:left">
+
                             	<?php if($wix_feat_image){ ?>
-                                <a href="<?php the_permalink(); ?>"><img src="<?php echo $wix_feat_image; ?>" alt="banner" /></a>
+                                <a href="<?php the_permalink(); ?>"><img width="300px" height="90px" src="<?php echo $wix_feat_image; ?>" alt="banner" /></a>
 								<?php } else { ?>
                                 <a href="<?php the_permalink(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/no-image.png" alt="banner" /></a>
                                 <?php } ?>
@@ -31,23 +40,24 @@ get_header();
                                     </div>
                                 </div>
                             </div>                               
-                            <div class="clearfix"></div>
-                            <div class="post-box-details">
+                           
+                            <div class="post-box-details" style="margin-left:10px;width:210px;float:left">
                             <a href="<?php the_permalink(); ?>"><?php  the_title(); ?></a>
-                               <?php  the_excerpt(); ?>
+                               <?php  //the_excerpt(); ?>
                             </div>
-                             <div class="post-box-link">
+                             <div class="post-box-link" style="width:300px;float:left">
                         		<ul>
                                 	<?php wix_entry_meta();?>
                                </ul>
                             </div>                            
                         </div>
                     </div>        
+
                 <?php endwhile; endif; // end have_posts() ?>    			
                     </div>
       
                     <!--Pagination Start-->
-                    <?php include_once( ABSPATH . 'wp-admin/includes/plugin.php' ); ?>
+                    <!--<?php include_once( ABSPATH . 'wp-admin/includes/plugin.php' ); ?>
                     <?php if(is_plugin_active('faster-pagination/ft-pagination.php')) {?>
                         <?php faster_pagination();?>
                     <?php }else { ?>
@@ -61,8 +71,55 @@ get_header();
                     <?php } ?>
                     <?php }//is plugin active ?>
                     <!--Pagination End-->
- 
+                <h1>
+                    Chủ đề mới 
+                    <span class="arrow"></span>
+                </h1>
+                <div class="detail-inner masonry-container">
+                <?php  if ( have_posts() ) :  while ( have_posts() ) : the_post(); ?>  
+                        <?php 
+                            $style = ($i==5 || $i==6) ? "":"border-bottom: 1px solid #e6e6e6;";
+                            $i++;
+                        ?>            
+                    <div class="topic">
+                        <div class="article">                             
+                           
+                            <div style="<?php echo $style; ?>margin-left:10px;width:100%;float:left;margin-bottom:10px">
+                           <div class="StatBox AnswersBox HasAnswersBox">
+                            <span>Comments</span>
+                            18
+                            </div>
+                            <div class="StatBox ViewsBox">
+                            <span>Views</span>
+                            356
+                            </div>
+                            <div class="ItemContent Discussion">
+                                <a style="color:#282828" href="<?php the_permalink(); ?>"><?php  the_title(); ?></a>
+                            <div class="Meta">
+                            <span class="Announcement">Announcement</span>
+                            <span class="CommentCount">19 comments</span>
+                            <span class="LastCommentBy">
+                            Most recent by
+                                <a href="/index.php?p=/profile/15732/SreenivasKanumuru">SreenivasKanumuru</a>
+                            </span>
+                            <span class="LastCommentDate">July 22</span>
+                            <span>
+                                <a class="Category" href="/index.php?p=/categories/vtiger-crm-mobile-apps">vtiger CRM - Mobile Apps</a>
+                            </span>
+                            </div>
+                            </div>
+                               <?php  //the_excerpt(); ?>
+                            </div>                       
+                        </div>
+                    </div>        
+
+                <?php endwhile; endif; // end have_posts() ?>               
+                    </div>
             </article>
+            <?php
+             get_sidebar();
+            ?>
+
         </div>
     </div>    
 </section>
