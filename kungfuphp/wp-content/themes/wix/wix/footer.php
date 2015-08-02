a730779b3b1edb509c37beb1c9068e47d2ed0798
hthieu1991 <programming.iter@gmail.com>
2015-08-02 15:18:16 +0700

    Add slider jQuery

--- kungfuphp/wp-content/themes/wix/wix/footer.php
+++ kungfuphp/wp-content/themes/wix/wix/footer.php
@@ -18,14 +18,7 @@
 
 		?>
 
-        </div>    
-
- 
-
+        </div>
 </footer>
-
-
-
 </body>
-
 </html>
---
+++ kungfuphp/wp-content/themes/wix/wix/js/slider_custom.js
@@ -0,0 +1,11 @@
+// jQuery(document).ready(function($) {
+// 	console.log($('#mi-slider'));
+// 	$('#mi-slider').catslider();
+// });
+jQuery(document).ready(function($) {
+$('.yourclass').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
+});
+});
