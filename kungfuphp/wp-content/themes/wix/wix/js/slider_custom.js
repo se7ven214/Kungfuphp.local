afda2839ef975e7ce44a2a551c5b0b928ca038e9
hthieu1991 <programming.iter@gmail.com>
2015-08-02 15:25:07 +0700

    cac

--- kungfuphp/wp-content/themes/wix/wix/js/slider_custom.js
+++ kungfuphp/wp-content/themes/wix/wix/js/slider_custom.js
@@ -6,6 +6,8 @@ jQuery(document).ready(function($) {
 $('.yourclass').slick({
   infinite: true,
   slidesToShow: 3,
-  slidesToScroll: 3
+  slidesToScroll: 3,
+  autoplay : true,
+  autoplaySpeed : 1000
 });
 });
