3e953f79f9b318676b72e17ed3fbc58490aa72d2
dtnhat <dtnhat@brights.vn>
2015-08-02 15:29:20 +0700

    footer

--- kungfuphp/wp-content/themes/wix/wix/footer.php
+++ kungfuphp/wp-content/themes/wix/wix/footer.php
@@ -3,25 +3,34 @@
 
 
     	<div class="footer">
-
-    	<?php 
-
-			$wix_options = get_option( 'wix_theme_options' );
-
-			if(!empty($wix_options['footertext'])) {
-
-				echo wp_filter_nohtml_kses($wix_options['footertext']);
-
-			}
-
-			 echo "Đối tác : <a href='http://shopnhoibong.com' rel='external nofollow' target='_blank' title='Shop Nhồi Bông'>Shop Nhồi Bông</a>&nbsp; .Copyright 09-11-2014 © by <a href='http://kungfuphp.com' title='học php online miễn phí'>Kungfu PHP</a> Version 2.0.";	
-
-		?>
-
+    	<div class="top-footer" style="height:30%;border-top:3px solid #BB2828;">
+			<article class="col-md-3">
+				<h4>NHAT</h4>
+				<img src="http://thachpham.com/wp-content/banners/do-logo.png"/>
+				<img src="http://thachpham.com/wp-content/banners/namecheap-logo.png"/>
+				<img src="http://thachpham.com/wp-content/banners/sucuri-logo.png"/>
+				<img src="https://logos.keycdn.com/keycdn-logo-white.png"/>
+			</article>
+			<article class="col-md-3">
+				<h4>NHAT</h4>
+			</article>
+			<article class="col-md-3">
+				<h4>NHAT</h4>
+			</article>
+			<article class="col-md-3">
+				<h4>NHAT</h4>
+			</article>
+		
+		
+		<article class="col-md-12">
+	    	<?php 
+	
+				 echo "Đối tác : <a href='http://shopnhoibong.com' rel='external nofollow' target='_blank' title='Shop Nhồi Bông'>Shop Nhồi Bông</a>&nbsp; .Copyright 09-11-2014 © by <a href='http://kungfuphp.com' title='học php online miễn phí'>Kungfu PHP</a> Version 2.0.";	
+	
+			?>
+		</article>
+		</div>
         </div>    
-
- 
-
 </footer>
 
 
